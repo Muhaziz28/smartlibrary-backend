0e33866fd327150cad5ac6e7a8540ee88838422e
Muhammad Haikal Aziz <haikal@Muhammads-MacBook-Pro.local>
2023-09-08 20:20:12 +0700

    periode mata kuliah

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,13 +11,14 @@ import { UserModule } from './user/user.module';
 import { PeriodeModule } from './periode/periode.module';
 import { AdminModule } from './admin/admin.module';
 import { MataKuliahModule } from './mata-kuliah/mata-kuliah.module';
+import { SesiMataKuliahModule } from './sesi-mata-kuliah/sesi-mata-kuliah.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    AuthModule, MahasiswaModule, DosenModule, PrismaModule, FakultasModule, ProdiModule, UserModule, PeriodeModule, AdminModule, MataKuliahModule],
+    AuthModule, MahasiswaModule, DosenModule, PrismaModule, FakultasModule, ProdiModule, UserModule, PeriodeModule, AdminModule, MataKuliahModule, SesiMataKuliahModule],
   controllers: [UserController],
   providers: [],
 })
