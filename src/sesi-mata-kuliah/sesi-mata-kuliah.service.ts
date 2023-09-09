9efcff95fcd647b74cd777bea5e5628c45d55fc3
Muhammad Haikal Aziz <haikal@Muhammads-MacBook-Pro.local>
2023-09-09 11:11:00 +0700

    sesi mata kuliah model

--- src/sesi-mata-kuliah/sesi-mata-kuliah.service.ts
+++ src/sesi-mata-kuliah/sesi-mata-kuliah.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class SesiMataKuliahService {}
+export class SesiMataKuliahService {
+    constructor(private prisma: PrismaService) { }
+
+
+}
